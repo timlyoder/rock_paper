cb7acd91f493cda7567ba5c7c2199489cbdfd56a
Tim <spoken@MacBook-Air.local>
2025-07-15 18:18:07 -0400

    update

--- rock.js
+++ rock.js
@@ -1,11 +1,44 @@
 
 
-var a = 1
+var a = "rock"
+var b = "paper"
+var c = "scissors"
+
+var random = Math.random();
+
+let humanScore = 0;
+let computerScore = 0;
 
 
-    function getComputerChoice() {
-        return a 
-    } 
+function getComputerChoice() {
+
+    if (random < .33) {
+        return a;
+
+    } else if  (random >=0.34 && random <=0.66  )  {
     
-console.log(getComputerChoice())
+        return b;
 
+    } else {
+        return c;
+    }   
+
+} 
+
+
+function getHumanChoice() {
+    let alert = prompt("choose rock, paper, or scissors");
+
+    if (alert = "rock", "paper", "scissors")
+
+        return getComputerChoice()
+}
+
+
+
+function playRound(humanChoice, computerChoice) {
+    
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
